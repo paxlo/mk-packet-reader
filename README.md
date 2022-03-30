6620dba29bebe02810c8893bad0deb4d5d4c3d5c
paxlo <{ID}+{username}@users.noreply.github.com>
2022-03-30 13:54:25 +0300

    11

--- README.md
+++
@@ -1,3 +0,0 @@
-#mk-packet-reader 
-
-Monitoring the consumption of the network in real time.
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export DISPLAY=:0
-gnome-terminal -e "python mk-packet-reader.py" --full-screen --zoom=1.5 --hide-menubar
